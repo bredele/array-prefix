153e35fa90c7fb5e54fda69f281bc93c508cb793
bredele <olivier.wietrich@gmail.com>
2018-11-21 10:49:18 -0700

    Filter and sort array with given string

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 
 module.exports = function (prefix, array, sort) {
+  const length = prefix.length
   const output = array.filter(function(item) {
-    return  item.indexOf(prefix) > -1
+    return prefix === item.substring(0, length)
   })
   return sort ? output.sort() : output
 }
